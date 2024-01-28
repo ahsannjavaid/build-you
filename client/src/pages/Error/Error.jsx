26c57cbd4de08577c490e7121baf2fe6aaf4100e
Ahsan Javed <ahsannjavaid@gmail.com>
2024-01-28 23:05:56 +0500

    error page update

--- client/src/pages/Error/Error.jsx
+++ client/src/pages/Error/Error.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Error() {
+  const navigate = useNavigate();
+
   return (
     <div className="d-flex justify-content-center align-items-center text-danger vh-100">
-        <div className="text-center">
-      <h6>Page not found!</h6>
-      <h1>ERR: 404</h1>
+      <div className="text-center">
+        <h6>Page not found!</h6>
+        <h1>ERR: 404</h1>
+        <div className="p-5">
+          <button onClick={() => navigate(-1)} className="btn btn-secondary">
+            Go Back
+          </button>
+        </div>
       </div>
     </div>
   );
